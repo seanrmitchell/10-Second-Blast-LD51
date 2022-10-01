318440880cb093722b3ac5afdee3e36a0171835c
seanmitchell <seanrmitchell211@gmail.com>
2022-10-01 17:47:16 -0400

    v0.1.1 Movement and CM Camera
    Adds Player Movement, Cam Followers, sprites, palettes, etc.

---
+++ 10 Second Blast 2.0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Controller playerController;
+
+    [SerializeField] //visible in editor
+    private float maxSpeed, speed;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private LayerMask groundObjects;
+    private Camera cam;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    Vector2 movement;
+
+    private void Awake()
+    {
+        playerController = new Controller();
+    }
+    private void OnEnable()
+    {
+        playerController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerController.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flip();
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Gets horizontal input
+        movement.y = playerController.Movement.WS.ReadValue<float>();
+        movement.x = playerController.Movement.AD.ReadValue<float>();
+
+
+        // calculates movement on ground
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        //Vector2 playerMove = new Vector2(Mathf.Clamp(movement * speed, -maxSpeed, maxSpeed), rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (rb.velocity.x > 0f)
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        if (rb.velocity.x < 0f)
+            gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
+
+    }
+
+    private void Attack(int x)
+    {
+        switch (x)
+        {
+            case 1:
+                Debug.Log("Primary Attack!");
+                break;
+            case 2:
+                Debug.Log("Special Attack!");
+                break;
+        }
+    }
+
+
+}
+
